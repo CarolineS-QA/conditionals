f158724f8fb0edd9c3232eceb932b3e67afe5cc5
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2020-03-25 12:59:55 +0000

    part 3

--- src/com/qa/main/ConditionalsExercise.java
+++ src/com/qa/main/ConditionalsExercise.java
@@ -2,55 +2,69 @@ package com.qa.main;
 
 public class ConditionalsExercise {
 
-//	public int sumOrMultiply(int number, int secondNumber, boolean sumOrProduct) {
-//		if (sumOrProduct == true) {
-//			int sum = number + secondNumber;
-//			System.out.println("Working out sum...\n" + "Sum is: " + sum);
-//			return (sum); // could change method to void to return nothing
-//		} else {
-//			int product = number * secondNumber;
-//			System.out.println("Working out product...\n" + "Product is: " + product);
-//			return (product);
-//		}
-//
-//	}
-	public void appendixAFlowChart(int start) {
-		if (start > 2000) {
-			// if the input is more than 2000, A will appear in the console
-			System.out.println("A - your input is more that 2000... (sorting)");
-			if (start > 6000) {
-				// if the input if more than 6000, a C will appear after A
-				System.out.println("C - your input is more than 6000. \n END");
+	// part 1 and 3
+	public int sumOrMultiply(int number, int secondNumber, boolean sumOrProduct) {
+		if (sumOrProduct == true) {
+			int sum = number + secondNumber;
+			System.out.println("Working out sum...\n" + "Sum is: " + sum);
+			if (number % 2 == 0 || secondNumber % 2 == 0) {
+				System.out.println("At least one of your numbers is even.");
+				// if firstNumber OR secondNumber has no remainder (equal to zero) its even
+				return 0;
 			} else {
-				// otherwise if its more than 2000 but less than 6000, a B will appear
-				System.out.println("B - your input is between 2000 and 6000... (sorting)");
-				if (start > 4000) {
-					// after printing a B it will sort for more or less than 4000
-					System.out.println("D - your input is between 4000 and 6000. \n END");
-				} else {
-					System.out.println("E - your input is between 2000 and 4000. \n END");
-				}
+				return (sum);
 			}
 		} else {
-			// input is less than 2000
-			System.out.println("1 - your input is less than 2000... (sorting)");
-			if (start > 100) {
-				System.out.println("3 - your input is between 100 and 2000... (sorting)");
-				if (start > 600) {
-					System.out.println("5 - your input is between 600 and 2000. \n END");
-				} else {
-					System.out.println("4 - your input is between 100 and 600... (sorting)");
-					if (start > 500) {
-						System.out.println("6 - your input is between 500 and 600. \n END");
-					} else {
-						System.out.println("7 - your input is between 100 and 500. \n END");
-					}
-				}
+			int product = number * secondNumber;
+			System.out.println("Working out product...\n" + "Product is: " + product);
+			if (number % 2 == 0 || secondNumber % 2 == 0) {
+				System.out.println("At least one of your numbers is even.");
+				// if firstNumber OR secondNumber has no remainder (equal to zero) its even
+				return 0;
 			} else {
-				System.out.println("2 - your input is less than 100. \n END");
+				return (product);
 			}
 		}
 
 	}
+	// part 2 appendix A method
+//	public void appendixAFlowChart(int start) {
+//		if (start > 2000) {
+//			// if the input is more than 2000, A will appear in the console
+//			System.out.println("A - your input is more that 2000... (sorting)");
+//			if (start > 6000) {
+//				// if the input if more than 6000, a C will appear after A
+//				System.out.println("C - your input is more than 6000. \n END");
+//			} else {
+//				// otherwise if its more than 2000 but less than 6000, a B will appear
+//				System.out.println("B - your input is between 2000 and 6000... (sorting)");
+//				if (start > 4000) {
+//					// after printing a B it will sort for more or less than 4000
+//					System.out.println("D - your input is between 4000 and 6000. \n END");
+//				} else {
+//					System.out.println("E - your input is between 2000 and 4000. \n END");
+//				}
+//			}
+//		} else {
+//			// input is less than 2000
+//			System.out.println("1 - your input is less than 2000... (sorting)");
+//			if (start > 100) {
+//				System.out.println("3 - your input is between 100 and 2000... (sorting)");
+//				if (start > 600) {
+//					System.out.println("5 - your input is between 600 and 2000. \n END");
+//				} else {
+//					System.out.println("4 - your input is between 100 and 600... (sorting)");
+//					if (start > 500) {
+//						System.out.println("6 - your input is between 500 and 600. \n END");
+//					} else {
+//						System.out.println("7 - your input is between 100 and 500. \n END");
+//					}
+//				}
+//			} else {
+//				System.out.println("2 - your input is less than 100. \n END");
+//			}
+//		}
+//
+//	}
 
 }
--- src/com/qa/main/Runner.java
+++ src/com/qa/main/Runner.java
@@ -3,22 +3,22 @@ package com.qa.main;
 public class Runner {
 
 	public static void main(String[] args) {
-//		// variables to modify for part 1
-//		int firstNumber = 3;
-//		int secondNumber = 2;
-//		Boolean trueToSumFalseForProduct = false;
+		// variables to modify for part 1
+		int firstNumber = 4;
+		int secondNumber = 5;
+		Boolean trueToSumFalseForProduct = false;
 
-		// variables for part 2 (Appendix A)
-		int inputA = 450;
+//		// variables for part 2 (Appendix A)
+//		int inputA = 450;
 
 		// instance class to use methods
 		ConditionalsExercise callMethod = new ConditionalsExercise();
 
 		// part 1 and 3 method
-//		callMethod.sumOrMultiply(firstNumber, secondNumber, trueToSumFalseForProduct);
+		callMethod.sumOrMultiply(firstNumber, secondNumber, trueToSumFalseForProduct);
 
-		// part 2 appendix method
-		callMethod.appendixAFlowChart(inputA);
+//		// part 2 appendix method
+//		callMethod.appendixAFlowChart(inputA);
 	}
 
 }
